f827b21cd643dff2471b24206da6de787e29637c
Gukson <75785907+Gukson@users.noreply.github.com>
2024-03-07 00:47:49 +0100

    ...

---
+++ heap_sort/heap_sort.h
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class HeapSort{
+    public:
+        vector<vector<int> > data;
+
+        void heapSort();
+        void testHeapSort();
+};
